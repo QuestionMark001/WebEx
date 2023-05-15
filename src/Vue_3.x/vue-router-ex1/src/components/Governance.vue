fdca272b34fed5cd111b41eb7ca36de35d109c50
QuestionMark001 <604661720@qq.com>
2023-05-15 16:11:23 +0800

    Updated Vue Router Ex1

---
+++ src/Vue_3.x/vue-router-ex1/src/components/Governance.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: QuestionMark001
+ * @Date: 2023-05-15 14:27:10
+ * @LastEditors: QuestionMark001
+ * @LastEditTime: 2023-05-15 15:44:11
+ * @FilePath: /LocalProjects/WebEx/src/Vue_3.x/vue-router-ex1/src/components/Governance.vue
+ * @Description: 关于页-公司治理 组件
+ * 
+ * Copyright (c) 2023 by QuestionMark001, All Rights Reserved. 
+-->
+
+<script setup></script>
+
+<template>
+<p>公司坚持以客户为中心... ...</p>
+</template>
+
+<style scoped></style>
