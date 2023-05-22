d7df64f2e26aef15e5e1c99dbff33f15485abe65
QuestionMark001 <604661720@qq.com>
2023-05-22 09:26:39 +0800

    Updated Vue Router Ex1

--- src/Vue_3.x/vue-router-ex1/src/router/index.js
+++ src/Vue_3.x/vue-router-ex1/src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: QuestionMark001
  * @Date: 2023-05-15 13:44:22
  * @LastEditors: QuestionMark001
- * @LastEditTime: 2023-05-15 14:47:46
+ * @LastEditTime: 2023-05-22 09:24:37
  * @FilePath: /LocalProjects/WebEx/src/Vue_3.x/vue-router-ex1/src/router/index.js
  * @Description: 路由配置
  * 
@@ -10,10 +10,11 @@
  */
 
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '../components/About.vue'
-import Contact from '../components/Contact.vue'
-import Detail from '../components/Detail.vue'
-import Governance from '../components/Governance.vue'
+// @ 是 Vue 中的特殊写法，代表当前项目下的 'src' 源代码文件夹
+import About from '@/components/About.vue'
+import Contact from '@/components/Contact.vue'
+import Detail from '@/components/Detail.vue'
+import Governance from '@/components/Governance.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
